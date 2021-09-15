7a280cc189926eefb8a67ee03b379b6eef53cbe0
moshe212 <moshe212@gmail.com>
2021-09-15 14:41:53 +0300

    f

--- footballFunc/index.js
+++ footballFunc/index.js
@@ -1,7 +1,7 @@
-const GetDataFromSheet = require("./getDataFromSheet");
+const getDataFromSheet = require("./GetDataFromSheet");
 const getCycle = require("./getCycle.js");
 
-const footballFunc = { GetDataFromSheet, getCycle };
+const footballFunc = { getDataFromSheet, getCycle };
 
 module.exports = { footballFunc };
 // module.exports = { connectToDB };
