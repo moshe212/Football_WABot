de0fc7cc1fd7ef59b9ec69996de1f3952c806d90
moshe212 <moshe212@gmail.com>
2021-09-17 16:02:24 +0300

    gh

--- footballFunc/getCycle.js
+++ footballFunc/getCycle.js
@@ -2,6 +2,7 @@ const moment = require("moment");
 
 const getCycle = (data) => {
   let isBetween = false;
+  let cycleNum = "";
   for (let i = 0; i < data.length; i++) {
     // console.log(Data[i]);
     const startDate = moment(
@@ -10,7 +11,7 @@ const getCycle = (data) => {
     );
     const endDate = moment(data[i]._rawData[1].replace("/", "-"), "DD-MM-YYYY");
     const CurrentDate = moment();
-    const cycleNum = data[i]._rawData[2];
+    cycleNum = data[i]._rawData[2];
 
     if (
       moment(CurrentDate).isBetween(startDate, endDate, undefined, "(]") ||
