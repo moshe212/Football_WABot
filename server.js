af8c9e7be9294b04f4ac1f3facc87e3500901d65
moshe212 <moshe212@gmail.com>
2021-10-07 21:42:53 +0300

    ב

--- server.js
+++ server.js
@@ -80,6 +80,7 @@ const getData = async () => {
   UsersIndex = await footballFunc.getDataFromSheet("אינדקס משתמשים");
   for (let l = 0; l < UsersIndex.length; l++) {
     if (user_name === UsersIndex[l]._rawData[0]) {
+      UsersList.push(UsersIndex[l]._rawData[0]);
       const first = UsersIndex[l]._rawData[2];
       if (first === "1") {
         isFirst = false;
