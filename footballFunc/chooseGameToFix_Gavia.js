cb2fcccc579f1c37875b2440d182bbac28748b2a
moshe212 <moshe212@gmail.com>
2021-11-23 15:59:39 +0200

    f

--- footballFunc/chooseGameToFix_Gavia.js
+++ footballFunc/chooseGameToFix_Gavia.js
@@ -3,8 +3,26 @@ const chooseGameToFix_Gavia = async function (
   message2,
   cycleNum
 ) {
+  let fullStr = "";
+  for (let i = 0; i < GuessData_Saved.length; i++) {
+    const str =
+      GuessData_Saved[i].team1[0] +
+      " - " +
+      GuessData_Saved[i].team2[0] +
+      " " +
+      GuessData_Saved[i].team1[1] +
+      ":" +
+      GuessData_Saved[i].team2[1] +
+      "\n*" +
+      GuessData_Saved[i].teamUp +
+      "* עולה, ב-" +
+      GuessData_Saved[i].minute +
+      "\n";
+
+    fullStr = fullStr + str;
+  }
   const textMessage1 = message2
-    ? "בחרתם לשנות אחד או יותר מניחושי המחזור ה-" +
+    ? "בחרתם לשנות אחד או יותר מניחושי שלב-" +
       cycleNum +
       "." +
       " בחרו את המשחק והניחוש אותו תרצו לשנות." +
@@ -71,65 +89,229 @@ const chooseGameToFix_Gavia = async function (
       GuessData_Saved[6].team1[1] +
       ":" +
       GuessData_Saved[6].team2[1]
-    : "*להלן הניחושים שלכם למחזור ה-" +
-      cycleNum +
-      ":*" +
-      "\n" +
-      GuessData_Saved[0].team1[0] +
-      " - " +
-      GuessData_Saved[0].team2[0] +
-      " " +
-      GuessData_Saved[0].team1[1] +
-      ":" +
-      GuessData_Saved[0].team2[1] +
-      "\n" +
-      GuessData_Saved[1].team1[0] +
-      " - " +
-      GuessData_Saved[1].team2[0] +
-      " " +
-      GuessData_Saved[1].team1[1] +
-      ":" +
-      GuessData_Saved[1].team2[1] +
-      "\n" +
-      GuessData_Saved[2].team1[0] +
-      " - " +
-      GuessData_Saved[2].team2[0] +
-      " " +
-      GuessData_Saved[2].team1[1] +
-      ":" +
-      GuessData_Saved[2].team2[1] +
-      "\n" +
-      GuessData_Saved[3].team1[0] +
-      " - " +
-      GuessData_Saved[3].team2[0] +
-      " " +
-      GuessData_Saved[3].team1[1] +
-      ":" +
-      GuessData_Saved[3].team2[1] +
-      "\n" +
-      GuessData_Saved[4].team1[0] +
-      " - " +
-      GuessData_Saved[4].team2[0] +
-      " " +
-      GuessData_Saved[4].team1[1] +
-      ":" +
-      GuessData_Saved[4].team2[1] +
-      "\n" +
-      GuessData_Saved[5].team1[0] +
-      " - " +
-      GuessData_Saved[5].team2[0] +
-      " " +
-      GuessData_Saved[5].team1[1] +
-      ":" +
-      GuessData_Saved[5].team2[1] +
-      "\n" +
-      GuessData_Saved[6].team1[0] +
-      " - " +
-      GuessData_Saved[6].team2[0] +
-      " " +
-      GuessData_Saved[6].team1[1] +
-      ":" +
-      GuessData_Saved[6].team2[1];
+    : "*להלן הניחושים שלכם לשלב-" + cycleNum + ":*" + "\n" + fullStr;
+  //     GuessData_Saved[0].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[0].team2[0] +
+  //     " " +
+  //     GuessData_Saved[0].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[0].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[0].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[0].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[1].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[1].team2[0] +
+  //     " " +
+  //     GuessData_Saved[1].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[1].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[1].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[1].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[2].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[2].team2[0] +
+  //     " " +
+  //     GuessData_Saved[2].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[2].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[2].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[2].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[3].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[3].team2[0] +
+  //     " " +
+  //     GuessData_Saved[3].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[3].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[3].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[3].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[4].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[4].team2[0] +
+  //     " " +
+  //     GuessData_Saved[4].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[4].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[4].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[4].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[5].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[5].team2[0] +
+  //     " " +
+  //     GuessData_Saved[5].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[5].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[5].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[5].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[6].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[6].team2[0] +
+  //     " " +
+  //     GuessData_Saved[6].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[6].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[6].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[6].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[7].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[7].team2[0] +
+  //     " " +
+  //     GuessData_Saved[7].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[7].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[7].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[7].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[8].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[8].team2[0] +
+  //     " " +
+  //     GuessData_Saved[8].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[8].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[8].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[8].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[9].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[9].team2[0] +
+  //     " " +
+  //     GuessData_Saved[9].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[9].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[9].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[9].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[10].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[10].team2[0] +
+  //     " " +
+  //     GuessData_Saved[10].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[10].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[10].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[10].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[11].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[11].team2[0] +
+  //     " " +
+  //     GuessData_Saved[11].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[11].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[11].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[11].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[12].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[12].team2[0] +
+  //     " " +
+  //     GuessData_Saved[12].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[12].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[12].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[12].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[13].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[13].team2[0] +
+  //     " " +
+  //     GuessData_Saved[13].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[13].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[13].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[13].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[14].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[14].team2[0] +
+  //     " " +
+  //     GuessData_Saved[14].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[14].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[14].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[14].minute +
+  //     "\n" +
+  //     //-----end game---
+  //     "\n" +
+  //     GuessData_Saved[15].team1[0] +
+  //     " - " +
+  //     GuessData_Saved[15].team2[0] +
+  //     " " +
+  //     GuessData_Saved[15].team1[1] +
+  //     ":" +
+  //     GuessData_Saved[15].team2[1] +
+  //     "\n*" +
+  //     GuessData_Saved[15].teamUp +
+  //     "* עולה, ב-" +
+  //     GuessData_Saved[15].minute;
+  // //-----end game---
 
   const textMessage2 = message2
     ? "יש להשיב במבנה הבא: " + "*משחק 1 תוצאה 2:1*"
--- footballFunc/getSavedGuss_Gavia.js
+++ footballFunc/getSavedGuss_Gavia.js
@@ -44,10 +44,10 @@ const getSavedGuss_Gavia = async function (
           const Team2 = GamesList[g][1];
           switch (g) {
             case 0:
-              score1 = SavedGuess[4];
-              score2 = SavedGuess[3];
-              teamUp = SavedGuess[5];
-              minute = SavedGuess[6];
+              score1 = SavedGuess[5];
+              score2 = SavedGuess[6];
+              teamUp = SavedGuess[7];
+              minute = SavedGuess[8];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
@@ -58,56 +58,197 @@ const getSavedGuss_Gavia = async function (
               //   console.log(Arr);
               break;
             case 1:
-              score1 = SavedGuess[16];
-              score2 = SavedGuess[15];
+              score1 = SavedGuess[21];
+              score2 = SavedGuess[22];
+              teamUp = SavedGuess[23];
+              minute = SavedGuess[24];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
               };
               Arr.push(scoresObj);
               break;
             case 2:
-              score1 = SavedGuess[28];
-              score2 = SavedGuess[27];
+              score1 = SavedGuess[37];
+              score2 = SavedGuess[38];
+              teamUp = SavedGuess[39];
+              minute = SavedGuess[40];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
               };
               Arr.push(scoresObj);
               break;
             case 3:
-              score1 = SavedGuess[40];
-              score2 = SavedGuess[39];
+              score1 = SavedGuess[53];
+              score2 = SavedGuess[54];
+              teamUp = SavedGuess[55];
+              minute = SavedGuess[56];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
               };
               Arr.push(scoresObj);
               break;
             case 4:
-              score1 = SavedGuess[52];
-              score2 = SavedGuess[51];
+              score1 = SavedGuess[69];
+              score2 = SavedGuess[70];
+              teamUp = SavedGuess[71];
+              minute = SavedGuess[72];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
               };
               Arr.push(scoresObj);
               break;
             case 5:
-              score1 = SavedGuess[64];
-              score2 = SavedGuess[63];
+              score1 = SavedGuess[85];
+              score2 = SavedGuess[86];
+              teamUp = SavedGuess[87];
+              minute = SavedGuess[88];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
               };
               Arr.push(scoresObj);
               break;
             case 6:
-              score1 = SavedGuess[76];
-              score2 = SavedGuess[75];
+              score1 = SavedGuess[101];
+              score2 = SavedGuess[102];
+              teamUp = SavedGuess[103];
+              minute = SavedGuess[104];
               scoresObj = {
                 team1: [Team1, score1],
                 team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 7:
+              score1 = SavedGuess[117];
+              score2 = SavedGuess[118];
+              teamUp = SavedGuess[119];
+              minute = SavedGuess[120];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 8:
+              score1 = SavedGuess[133];
+              score2 = SavedGuess[134];
+              teamUp = SavedGuess[135];
+              minute = SavedGuess[136];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 9:
+              score1 = SavedGuess[149];
+              score2 = SavedGuess[150];
+              teamUp = SavedGuess[151];
+              minute = SavedGuess[152];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 10:
+              score1 = SavedGuess[165];
+              score2 = SavedGuess[166];
+              teamUp = SavedGuess[167];
+              minute = SavedGuess[168];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 11:
+              score1 = SavedGuess[181];
+              score2 = SavedGuess[182];
+              teamUp = SavedGuess[183];
+              minute = SavedGuess[184];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 12:
+              score1 = SavedGuess[197];
+              score2 = SavedGuess[198];
+              teamUp = SavedGuess[199];
+              minute = SavedGuess[200];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 13:
+              score1 = SavedGuess[213];
+              score2 = SavedGuess[214];
+              teamUp = SavedGuess[215];
+              minute = SavedGuess[216];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 14:
+              score1 = SavedGuess[229];
+              score2 = SavedGuess[230];
+              teamUp = SavedGuess[231];
+              minute = SavedGuess[232];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
+              };
+              Arr.push(scoresObj);
+              break;
+            case 15:
+              score1 = SavedGuess[245];
+              score2 = SavedGuess[246];
+              teamUp = SavedGuess[247];
+              minute = SavedGuess[248];
+              scoresObj = {
+                team1: [Team1, score1],
+                team2: [Team2, score2],
+                teamUp: teamUp,
+                minute: minute,
               };
               Arr.push(scoresObj);
               break;
