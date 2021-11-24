b20cfff3a320c73fd074b4f9356e84ad1c2e5464
moshe212 <moshe212@gmail.com>
2021-11-24 18:24:40 +0200

    ד

--- server.js
+++ server.js
@@ -319,61 +319,61 @@ app.post("/api/Whatsapp", async (req, res) => {
         Minuts = "פנדלים";
       }
 
-      // if (GamesList.length < 2) {
-      await footballFunc.saveData_Full(
-        user_name,
-        UsersIndex,
-        GuessData,
-        cycleIndexNum,
-        "גביע המדינה",
-        "I",
-        "",
-        "",
-        "",
-        "",
-        Minuts
-      );
-      GuessData_Saved = await footballFunc.getSavedGuss_Gavia(
-        user_name,
-        UsersIndex,
-        cycleIndexNum,
-        "גביע המדינה",
-        GamesList
-      );
-      console.log("GuessData_Saved", GuessData_Saved);
-      textMessage = await footballFunc.chooseGameToFix_Gavia(
-        GuessData_Saved,
-        false,
-        cycleNum
-      );
+      if (GamesList.length < 2) {
+        await footballFunc.saveData_Full(
+          user_name,
+          UsersIndex,
+          GuessData,
+          cycleIndexNum,
+          "גביע המדינה",
+          "I",
+          "",
+          "",
+          "",
+          "",
+          Minuts
+        );
+        GuessData_Saved = await footballFunc.getSavedGuss_Gavia(
+          user_name,
+          UsersIndex,
+          cycleIndexNum,
+          "גביע המדינה",
+          GamesList
+        );
+        console.log("GuessData_Saved", GuessData_Saved);
+        textMessage = await footballFunc.chooseGameToFix_Gavia(
+          GuessData_Saved,
+          false,
+          cycleNum
+        );
 
-      textMessage1 = textMessage[0];
-      textMessage2 = textMessage[1];
+        textMessage1 = textMessage[0];
+        textMessage2 = textMessage[1];
 
-      break;
-    // } else {
-    //   Team1 = GamesList[1][0];
-    //   Team2 = GamesList[1][1];
+        break;
+      } else {
+        Team1 = GamesList[1][0];
+        Team2 = GamesList[1][1];
 
-    //   const str11 = "*" + cycleNum + ", משחק מספר 2:* ";
-    //   textMessage1 = str11 + "\n" + Team1 + " - " + Team2;
-    //   textMessage2 = "מה תהיה תוצאת המשחק בתום הזמן החוקי?";
+        const str11 = "*" + cycleNum + ", משחק מספר 2:* ";
+        textMessage1 = str11 + "\n" + Team1 + " - " + Team2;
+        textMessage2 = "מה תהיה תוצאת המשחק בתום הזמן החוקי?";
 
-    //   footballFunc.saveData_Full(
-    //     user_name,
-    //     UsersIndex,
-    //     GuessData,
-    //     cycleIndexNum,
-    //     "גביע המדינה",
-    //     "I",
-    //     "",
-    //     "",
-    //     "",
-    //     "",
-    //     Minuts
-    //   );
-    //   break;
-    // }
+        footballFunc.saveData_Full(
+          user_name,
+          UsersIndex,
+          GuessData,
+          cycleIndexNum,
+          "גביע המדינה",
+          "I",
+          "",
+          "",
+          "",
+          "",
+          Minuts
+        );
+        break;
+      }
 
     case 122:
       console.log(GamesList);
