e205403ac82e62b23af27f1c9698b79d5db794d0
moshe212 <moshe212@gmail.com>
2021-12-01 16:40:17 +0200

    f

--- server.js
+++ server.js
@@ -317,7 +317,11 @@ app.post("/api/Whatsapp", async (req, res) => {
       const str10 = "*" + cycleNum + ", משחק מספר 1:* ";
       textMessage1 = str10 + "\n" + Team1 + " - " + Team2;
       textMessage2 = "איך יסתיים המשחק?";
-      textMessage3 = "\n1️⃣ 90 דקות \n2️⃣ 120 דקות \n3️⃣ פנדלים";
+      if (GameRow.data[0] != GameRow.data[1]) {
+        textMessage3 = "\n1️⃣ 90 דקות \n2️⃣ 120 דקות";
+      } else {
+        textMessage3 = "\n1️⃣ 90 דקות \n2️⃣ 120 דקות \n3️⃣ פנדלים";
+      }
 
       ChoiseUp = req.body.query.message;
       if (parseInt(ChoiseUp) === 1) {
