9f71e1b2f4f7b794f3730bb795c38799284e330f
moshe212 <moshe212@gmail.com>
2021-12-01 16:15:39 +0200

    d

--- footballFunc/getGameGuss.js
+++ footballFunc/getGameGuss.js
@@ -52,7 +52,7 @@ const getGameGuss = async function (
       const readData = await googleSheetsInstance.spreadsheets.values.get({
         auth, //auth object
         spreadsheetId, // spreadsheet id
-        range: "Sheet1!A:A", //range of cells to read from.
+        range: Range_Cell, //range of cells to read from.
       });
 
       console.log("save");
