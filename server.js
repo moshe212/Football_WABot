aab916c9115dc80b5308c51d8ea22f5ea885c170
moshe212 <moshe212@gmail.com>
2021-12-01 16:57:53 +0200

    d

--- server.js
+++ server.js
@@ -317,9 +317,9 @@ app.post("/api/Whatsapp", async (req, res) => {
       const str10 = "*" + cycleNum + ", משחק מספר 1:* ";
       textMessage1 = str10 + "\n" + Team1 + " - " + Team2;
       textMessage2 = "איך יסתיים המשחק?";
-      console.log(GameRow.data[0], GameRow.data[1]);
-      console.log(GameRow.data[0][0], GameRow.data[0][1]);
-      if (GameRow.data[0] != GameRow.data[1]) {
+      console.log(GameRow.data.values[0], GameRow.data.values[1]);
+      // console.log(GameRow.data[0][0], GameRow.data[0][1]);
+      if (GameRow.data.values[0] != GameRow.data.values[1]) {
         textMessage3 = "\n1️⃣ 90 דקות \n2️⃣ 120 דקות";
       } else {
         textMessage3 = "\n1️⃣ 90 דקות \n2️⃣ 120 דקות \n3️⃣ פנדלים";
