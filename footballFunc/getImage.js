13cdf2b3b391558707427dd8bb88d28dbee4e431
moshe212 <moshe212@gmail.com>
2022-08-11 16:04:15 +0300

    remark getUmage func

--- footballFunc/getImage.js
+++ footballFunc/getImage.js
@@ -1,12 +1,12 @@
-const nodeHtmlToImage = require("node-html-to-image");
+// const nodeHtmlToImage = require("node-html-to-image");
 
-const getImage = async () => {
-  const image = await nodeHtmlToImage({
-    html: "<html><body><div>Check out what I just did! #cool</div></body></html>",
-  });
-  //   res.writeHead(200, { 'Content-Type': 'image/png' });
-  //   res.end(image, 'binary');
-  return image;
-};
+// const getImage = async () => {
+//   const image = await nodeHtmlToImage({
+//     html: "<html><body><div>Check out what I just did! #cool</div></body></html>",
+//   });
+//   //   res.writeHead(200, { 'Content-Type': 'image/png' });
+//   //   res.end(image, 'binary');
+//   return image;
+// };
 
-module.exports = { getImage };
+// module.exports = { getImage };
