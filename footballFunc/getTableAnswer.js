79f284052a5ac76bc92bb4ca047cf10f4bee1e2e
moshe212 <moshe212@gmail.com>
2022-08-19 12:54:36 +0300

    fg

--- footballFunc/getTableAnswer.js
+++ footballFunc/getTableAnswer.js
@@ -1,15 +1,16 @@
 const getTableAnswer = async (tableName, tableObj) => {
   const isClalitTable = tableName === "clalitTable";
+  const start1 = isClalitTable
+    ? tableObj[tableName][0][3]
+    : tableObj[tableName][0][2];
+  const start2 = isClalitTable
+    ? tableObj[tableName][0][4]
+    : tableObj[tableName][0][3];
 
   let tableString = "";
   for (let i = 1; i < tableObj[tableName].length; i++) {
     // console.log(tableObj[tableName][i][1].trim(), length);
-    const start1 = isClalitTable
-      ? tableObj[tableName][0][3]
-      : tableObj[tableName][0][2];
-    const start2 = isClalitTable
-      ? tableObj[tableName][0][4]
-      : tableObj[tableName][0][3];
+
     const number = `${tableObj[tableName][i][0]}`;
     const name = isClalitTable
       ? `${tableObj[tableName][i][2]}`
