c0fa5131f0a91c5b6cbb1fc0eb312d11b84d6204
moshe212 <moshe212@gmail.com>
2023-10-19 15:15:19 +0300

    trigger deploy

--- server.js
+++ server.js
@@ -595,6 +595,7 @@ app.post("/api/Whatsapp", async (req, res) => {
     textMessage2 = OlamiMessages[1];
     textMessage3 = OlamiMessages[2];
   } else if (stage === 255 || stage > 800) {
+    console.log("NBAMessages");
     const NBAMessages = await botRollsFunctions.NBA(
       nbaCycleNum,
       nbaCycleText,
