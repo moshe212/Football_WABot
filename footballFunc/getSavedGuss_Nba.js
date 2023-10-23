1c60b799325920e6e85f512b76f285889bb04e39
moshe212 <moshe212@gmail.com>
2023-10-23 12:56:36 +0300

    d

--- footballFunc/getSavedGuss_Nba.js
+++ footballFunc/getSavedGuss_Nba.js
@@ -40,165 +40,180 @@ const getSavedGuss_Nba = async function ({
         console.log("idxss", raw_idx);
         const SavedGuess = GuessData[raw_idx]._rawData;
         console.log("SavedGuess", SavedGuess);
-        // let Arr = [];
-        // let score1 = 0;
-        // let score2 = 0;
-        // let teamUp = "";
-        // let scoresObj = {};
-        // for (let g = 0; g < GamesList.length; g++) {
-        //   console.log("g", g);
-        //   const Team1 = GamesList[g][0];
-        //   const Team2 = GamesList[g][1];
-        //   switch (g) {
-        //     case 0:
-        //       score1 = SavedGuess[5];
-        //       score2 = SavedGuess[6];
-        //       teamUp = SavedGuess[7];
-        //       minute = fileName === "Mondial" ? SavedGuess[8] : SavedGuess[7];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       //   console.log(Arr);
-        //       break;
-        //     case 1:
-        //       score1 = SavedGuess[21];
-        //       score2 = SavedGuess[22];
-        //       teamUp = SavedGuess[23];
-        //       minute = fileName === "Mondial" ? SavedGuess[24] : SavedGuess[23];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 2:
-        //       score1 = SavedGuess[37];
-        //       score2 = SavedGuess[38];
-        //       teamUp = SavedGuess[39];
-        //       minute = fileName === "Mondial" ? SavedGuess[40] : SavedGuess[39];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 3:
-        //       score1 = SavedGuess[53];
-        //       score2 = SavedGuess[54];
-        //       teamUp = SavedGuess[55];
-        //       minute = fileName === "Mondial" ? SavedGuess[56] : SavedGuess[55];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 4:
-        //       score1 = SavedGuess[69];
-        //       score2 = SavedGuess[70];
-        //       teamUp = SavedGuess[71];
-        //       minute = fileName === "Mondial" ? SavedGuess[72] : SavedGuess[71];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 5:
-        //       score1 = SavedGuess[85];
-        //       score2 = SavedGuess[86];
-        //       teamUp = SavedGuess[87];
-        //       minute = SavedGuess[88];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 6:
-        //       score1 = SavedGuess[101];
-        //       score2 = SavedGuess[102];
-        //       teamUp = SavedGuess[103];
-        //       minute = SavedGuess[104];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 7:
-        //       score1 = SavedGuess[117];
-        //       score2 = SavedGuess[118];
-        //       teamUp = SavedGuess[119];
-        //       minute = SavedGuess[120];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 8:
-        //       score1 = SavedGuess[133];
-        //       score2 = SavedGuess[134];
-        //       teamUp = SavedGuess[135];
-        //       minute = SavedGuess[136];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 9:
-        //       score1 = SavedGuess[149];
-        //       score2 = SavedGuess[150];
-        //       teamUp = SavedGuess[151];
-        //       minute = SavedGuess[152];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
-        //     case 10:
-        //       score1 = SavedGuess[165];
-        //       score2 = SavedGuess[166];
-        //       teamUp = SavedGuess[167];
-        //       minute = SavedGuess[168];
-        //       scoresObj = {
-        //         team1: [Team1, score1],
-        //         team2: [Team2, score2],
-        //         teamUp: teamUp,
-        //         minute: minute,
-        //       };
-        //       Arr.push(scoresObj);
-        //       break;
+        let Arr = [];
+        let score1 = 0;
+        let score2 = 0;
+        let teamUp = "";
+        let scoresObj = {};
+        for (let g = 0; g < GamesList.length; g++) {
+          console.log("g", g);
+          const Team1 = GamesList[g][0];
+          const Team2 = GamesList[g][1];
 
-        //   }
-        // }
-        // console.log(Arr);
-        // return Arr;
+          let baseNum = 3;
+          const Location = SavedGuess[baseNum];
+          const Difference = SavedGuess[baseNum + 2];
+          const Under_Over = SavedGuess[baseNum + 4];
+
+          const gameDetialsObj = {
+            Team1,
+            Team2,
+            Location,
+            Difference,
+            Under_Over,
+          };
+          Arr.push(gameDetialsObj);
+          baseNum = baseNum + 6;
+
+          // switch (g) {
+          //   case 0:
+          //     score1 = SavedGuess[5];
+          //     score2 = SavedGuess[6];
+          //     teamUp = SavedGuess[7];
+          //     minute = fileName === "Mondial" ? SavedGuess[8] : SavedGuess[7];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     //   console.log(Arr);
+          //     break;
+          //   case 1:
+          //     score1 = SavedGuess[21];
+          //     score2 = SavedGuess[22];
+          //     teamUp = SavedGuess[23];
+          //     minute = fileName === "Mondial" ? SavedGuess[24] : SavedGuess[23];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 2:
+          //     score1 = SavedGuess[37];
+          //     score2 = SavedGuess[38];
+          //     teamUp = SavedGuess[39];
+          //     minute = fileName === "Mondial" ? SavedGuess[40] : SavedGuess[39];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 3:
+          //     score1 = SavedGuess[53];
+          //     score2 = SavedGuess[54];
+          //     teamUp = SavedGuess[55];
+          //     minute = fileName === "Mondial" ? SavedGuess[56] : SavedGuess[55];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 4:
+          //     score1 = SavedGuess[69];
+          //     score2 = SavedGuess[70];
+          //     teamUp = SavedGuess[71];
+          //     minute = fileName === "Mondial" ? SavedGuess[72] : SavedGuess[71];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 5:
+          //     score1 = SavedGuess[85];
+          //     score2 = SavedGuess[86];
+          //     teamUp = SavedGuess[87];
+          //     minute = SavedGuess[88];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 6:
+          //     score1 = SavedGuess[101];
+          //     score2 = SavedGuess[102];
+          //     teamUp = SavedGuess[103];
+          //     minute = SavedGuess[104];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 7:
+          //     score1 = SavedGuess[117];
+          //     score2 = SavedGuess[118];
+          //     teamUp = SavedGuess[119];
+          //     minute = SavedGuess[120];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 8:
+          //     score1 = SavedGuess[133];
+          //     score2 = SavedGuess[134];
+          //     teamUp = SavedGuess[135];
+          //     minute = SavedGuess[136];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 9:
+          //     score1 = SavedGuess[149];
+          //     score2 = SavedGuess[150];
+          //     teamUp = SavedGuess[151];
+          //     minute = SavedGuess[152];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          //   case 10:
+          //     score1 = SavedGuess[165];
+          //     score2 = SavedGuess[166];
+          //     teamUp = SavedGuess[167];
+          //     minute = SavedGuess[168];
+          //     scoresObj = {
+          //       team1: [Team1, score1],
+          //       team2: [Team2, score2],
+          //       teamUp: teamUp,
+          //       minute: minute,
+          //     };
+          //     Arr.push(scoresObj);
+          //     break;
+          // }
+        }
+        console.log(Arr);
+        return Arr;
       } else {
         console.log("not find saved guess");
         return "not find saved guess";
