3bd14508711282e45b8f2739457b2aa1508feae2
moshe212 <moshe212@gmail.com>
2024-06-02 17:51:45 +0300

    fix9

--- footballDataFunc/getEuroData.js
+++ footballDataFunc/getEuroData.js
@@ -29,6 +29,7 @@ const getEuroData = async () => {
   );
   for (let g = 0; g < euroGames.length; g++) {
     console.log("euroGames[g]._rawData[0]", euroGames[g]._rawData[0]);
+    console.log("euroCycleNum", euroCycleNum);
     if (euroGames[g]._rawData[0] === euroCycleNum) {
       const team_1 = euroGames[g]._rawData[1];
       const team_2 = euroGames[g]._rawData[2];
